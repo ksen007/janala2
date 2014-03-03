bb97d98fd4800a836c5b186737021799d51f8eb2
ksen <ksen@cs.berkeley.edu>
2014-03-03 08:45:26 -0800

    python scripts for concolic testing.

---
+++ concolic.py
@@ -0,0 +1,83 @@
+import shutil
+import os
+import subprocess
+import platform
+import sys
+import shlex
+import argparse
+
+def getArguments ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--offline", help="Perform concolic testing offline.  An intermediate trace file is generated during the execution of the program. offilne mode results in 2X slowdown that non-offline mode", action="store_true")
+    parser.add_argument("maxIterations", help="Maximum number of times the program under test can be executed.", type=int)
+    parser.add_argument("className", help="Java class to be tested.")
+    parser.add_argument("arguments", nargs='*', help="Arguments passed to the program under test.")
+    args = parser.parse_args()
+    return args
+
+
+
+def concolic ():
+    if platform.system() == "Windows":
+        sep = ";"
+        windows=True
+    else:
+        sep = ":"
+        windows=False
+    catg_home = os.path.abspath(os.path.dirname(__file__)).replace("\\","/")+"/"
+    classpath = catg_home+"out/production/tests"+sep+catg_home+"out/production/janala"+sep+catg_home+"lib/asm-all-3.3.1.jar"+sep+catg_home+"lib/trove-3.0.3.jar"+sep+catg_home+"lib/automaton.jar"+sep+catg_home+"lib/iagent.jar"
+    args = getArguments()
+    iters = args.maxIterations
+    yourpgm = args.className
+    isOffline = args.offline
+    if isOffline:
+        loggerClass = "janala.logger.FileLogger"
+    else:
+        loggerClass = "janala.logger.DirectConcolicExecution"
+    arguments = ' '.join(args.arguments)
+    cmd1 = "java -Djanala.loggerClass="+loggerClass+" -Djanala.conf="+catg_home+"catg.conf -javaagent:\""+catg_home+"lib/iagent.jar\" -cp "+ classpath+" -ea "+yourpgm+" "+arguments
+    cmd1List = shlex.split(cmd1)
+
+    catg_tmp_dir = "catg_tmp"
+    try:
+        shutil.rmtree(catg_tmp_dir)
+    except: pass
+    os.mkdir(catg_tmp_dir)
+    os.chdir(catg_tmp_dir)
+
+    print "Now testing "+yourpgm
+
+    i = 1
+    while i <= iters:
+        try: 
+            shutil.copy("inputs", "inputs{}".format(i))
+            shutil.copy("inputs", "inputs.old")
+        except:
+            pass
+        try: 
+            shutil.copy("history", "history.old")
+        except:
+            pass
+        print "[Input {}]".format(i)
+        subprocess.call(cmd1List, shell=windows)
+        if isOffline:
+            print "..."
+            cmd2 = "java -Djanala.conf="+catg_home+"catg.conf -Djanala.mainClass="+yourpgm+" -Djanala.iteration="+str(i)+" -cp "+classpath+" -ea janala.interpreters.LoadAndExecuteInstructions"
+            cmd2List = shlex.split(cmd2)
+            subprocess.call(cmd2List, shell=windows)
+        i = i + 1
+        if os.path.isfile("history"):
+            pass
+        elif i == iters:
+            with open("../test.log", 'a') as f:
+                f.write("{} passed\n".format(yourpgm))
+            sys.exit()
+        else:
+            with open("../test.log", 'a') as f:
+                f.write("****************** {} failed!!!\n".format(yourpgm))
+            sys.exit()
+    with open("../test.log", 'a') as f:
+        f.write("****************** {} failed!!!\n".format(yourpgm))
+
+concolic()
+
---
+++ testall.py
@@ -0,0 +1,128 @@
+# Copyright (c) 2014, NTT Multimedia Communications Laboratories, Inc. and Koushik Sen
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+import argparse
+import os
+import subprocess
+import shlex
+import platform
+
+def getArguments ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--offline", help="Perform concolic testing offline.  An intermediate trace file is generated during the execution of the program. offilne mode results in 2X slowdown that non-offline mode", action="store_true")
+    args = parser.parse_args()
+    return args
+
+
+args = getArguments()
+try:
+    os.remove("test.log")
+except: pass
+    
+if args.offline:
+    argument = "--offline"
+else:
+    argument = ""
+
+
+tests = ["5 tests.test_q1_q4_min",
+    "9 tests.test_q1_q4",
+    "3 tests.test_q0_q4",
+    "3 tests.BoolFlow",
+    "2 tests.BoolFlow2",
+    "3 tests.BoolTest",
+    "5 tests.BoolTest2",
+    "5 tests.BoolTest3",
+    "3 tests.StringEquals",
+    "4 tests.StringTest2",
+    "4 tests.StringTest3",
+    "4 tests.StringTest4",
+    "3 tests.StringTest5",
+    "4 tests.StringTest6",
+    "4 tests.StringTest7",
+    "4 tests.StringTest8",
+    "4 tests.StringTest9",
+    "5 tests.StringSubStringTest2",
+    "6 tests.StringComplexTest1",
+    "4 tests.StringComplexTest3",
+    "3 tests.StringRegexTest1",
+    "3 tests.StringRegexTest2",
+    "3 tests.StringRegexTest3",
+    "5 tests.StringLengthTest1",
+    "3 tests.StringLengthTest2",
+    "3 tests.StringLengthTest3",
+    "3 tests.StringMapTest",
+    "4 tests.LongTest",
+    "7 tests.LongTest3",
+    "4 tests.TimestampTest",
+    "4 tests.Testme",
+    "4 tests.TestmeInteger",
+    "4 tests.TestmeLong",
+    "4 tests.NonLinear",
+    "5 tests.BoolTest2",
+    "8 tests.SwitchTest",
+    "3 tests.SwitchTest2",
+    "5 tests.Linear",
+    "3 tests.SimpleDBTest",
+    "17 tests.DBUpdateTest",
+    "17 tests.DBSelectTest",
+    "17 tests.DBDeleteTest",
+    "17 tests.DBDateSelectTest",
+    "6 tests.DBInsertTest",
+    "5 tests.OrAssumptionTest",
+    "4 tests.OrAssumptionTest2",
+    "6 tests.AssertIfPossibleTest1",
+    "10 tests.AssertIfPossibleTest2",
+    "97 tests.ManyColumnsOrRecords",
+    "10 tests.AbstractionTest1",
+    "14 tests.AbstractionTest2",
+    "119 tests.ManyColumnsRecords2",
+    "29 tests.QSort",
+    "29 tests.QSortLong",
+    "25 tests.InsertionSort",
+    "121 tests.MergeSortJDK15",
+    "76 tests.BinaryTreeSearch",
+    "13 tests.HeapInsertJDK15",
+    "25 tests.QuickSortJDK15",
+    "217 tests.RedBlackTreeSearch",
+    "121 tests.SortedListInsert",
+    "206 tests.BellmanFord",
+    "157 tests.Dijkstra",
+    "100 tests.BookStoreNoSQL"]
+
+
+if platform.system() == "Windows":
+    windows=True
+else:
+    windows=False
+
+for test in tests:
+    subprocess.call(shlex.split("python concolic.py "+argument+" "+test), shell=windows)
+
+f = open('test.log', 'r')
+print f.read()
+f.close()
+
